b85ed2867530b3052d836baa35cc17a3c20c0931
chmsly <chris.c.hemsley@gmail.com>
2024-05-15 14:11:24 -0600

    ticket 1 deliverables

---
+++ src/main/java/com/hcc/entities/Authority.java
@@ -0,0 +1,42 @@
+package com.hcc.entities;
+
+import javax.persistence.*;
+
+@Entity
+public class Authority {
+    private Long id;
+    private String authority;
+    @ManyToOne
+    private User user;
+
+    public Authority() { }
+
+    public Authority(String authority, User user) {
+        this.authority = authority;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/hcc/entities/User.java
@@ -0,0 +1,90 @@
+package com.hcc.entities;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class User implements UserDetails {
+
+    private Long id;
+    private LocalDate cohortStartDate;
+    private String username;
+    private String password;
+    private List<Authority> authorities;
+
+    public User() {
+    }
+
+    public User (LocalDate cohortStartDate, String username, String password, List<Authority> authorities) {
+        this.cohortStartDate = cohortStartDate;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(new Authority("role_student"));
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getCohortStartDate() {
+        return cohortStartDate;
+    }
+
+    public void setCohortStartDate(LocalDate cohortStartDate) {
+        this.cohortStartDate = cohortStartDate;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+    public void setAuthorities(List<Authority> authorities) {
+        this.authorities = authorities;
+    }
+}
